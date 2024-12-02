1c83f140b2e490cea566d4b79b0648e8d129e6ba
James Conigrave <james.conigrave@gmail.com>
2024-12-02 16:56:06 +1100

    match function names to data names, and ensure available

--- R/cleaning/cardio_index.R
+++ R/cleaning/cardio_index.R
@@ -27,7 +27,7 @@ get_cardio_index <- function(age = 11,
   # Systolic Blood Pressure
   if (!is.null(SBP)) {
     SP_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, `SBP (mmHg)`]
-    SP_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender,  `SBP (mmHg)`]
+    SP_sd <- bio_ref_data[Variable == "SD" & Sex == gender,  `SBP (mmHg)`]
     if(length(SP_mean) > 0 && length(SP_sd) > 0){
       SP_z <- calc_z(SBP, SP_mean, SP_sd)
       z_scores$SBP_z <- SP_z
@@ -37,7 +37,7 @@ get_cardio_index <- function(age = 11,
   # Diastolic Blood Pressure
   if (!is.null(DBP)) {
     DBP_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender,`DBP (mmHg)`]
-    DBP_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender,`DBP (mmHg)`]
+    DBP_sd <- bio_ref_data[Variable == "SD" & Sex == gender,`DBP (mmHg)`]
     if(length(DBP_mean) > 0 && length(DBP_sd) > 0){
       DBP_z <- calc_z(DBP, DBP_mean, DBP_sd)
       z_scores$DBP_z <- DBP_z
@@ -46,8 +46,8 @@ get_cardio_index <- function(age = 11,
   
   # BMI
   if (!is.null(BMI)) {
-    BMI_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, "BMI"]
-    BMI_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender, "BMI"]
+    BMI_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, `BMI (kg/m2)`]
+    BMI_sd <- bio_ref_data[Variable == "SD" & Sex == gender, `BMI (kg/m2)`]
     if(length(BMI_mean) > 0 && length(BMI_sd) > 0){
       BMI_z <- calc_z(BMI, BMI_mean, BMI_sd)
       z_scores$BMI_z <- BMI_z
@@ -56,28 +56,19 @@ get_cardio_index <- function(age = 11,
   
   # Waist Circumference
   if (!is.null(waist_circumference)) {
-    WC_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, "Waist_Circumference"]
-    WC_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender, "Waist_Circumference"]
+    WC_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, `WC (cm)`]
+    WC_sd <- bio_ref_data[Variable == "SD" & Sex == gender, `WC (cm)`]
     if(length(WC_mean) > 0 && length(WC_sd) > 0){
       WC_z <- calc_z(waist_circumference, WC_mean, WC_sd)
       z_scores$WaistCircumference_z <- WC_z
     }
   }
   
-  # Total Body Fat Percentage
-  if (!is.null(total_body_fat)) {
-    BF_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, "Total_Body_Fat"]
-    BF_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender, "Total_Body_Fat"]
-    if(length(BF_mean) > 0 && length(BF_sd) > 0){
-      BF_z <- calc_z(total_body_fat, BF_mean, BF_sd)
-      z_scores$TotalBodyFat_z <- BF_z
-    }
-  }
   
   # Total Cholesterol
   if (!is.null(total_cholesterol)) {
-    TC_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, "Total_Cholesterol"]
-    TC_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender, "Total_Cholesterol"]
+    TC_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, `TC (mmol/L)`]
+    TC_sd <- bio_ref_data[Variable == "SD" & Sex == gender, `TC (mmol/L)`]
     if(length(TC_mean) > 0 && length(TC_sd) > 0){
       TC_z <- calc_z(total_cholesterol, TC_mean, TC_sd)
       z_scores$TotalCholesterol_z <- TC_z
@@ -86,8 +77,8 @@ get_cardio_index <- function(age = 11,
   
   # LDL Cholesterol
   if (!is.null(LDL_C)) {
-    LDL_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, "LDL_C"]
-    LDL_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender, "LDL_C"]
+    LDL_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, `LDL-C (mmol/L)`]
+    LDL_sd <- bio_ref_data[Variable == "SD" & Sex == gender, `LDL-C (mmol/L)`]
     if(length(LDL_mean) > 0 && length(LDL_sd) > 0){
       LDL_z <- calc_z(LDL_C, LDL_mean, LDL_sd)
       z_scores$LDL_C_z <- LDL_z
@@ -96,32 +87,18 @@ get_cardio_index <- function(age = 11,
   
   # HDL Cholesterol
   if (!is.null(HDL_C)) {
-    HDL_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, "HDL_C"]
-    HDL_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender, "HDL_C"]
+    HDL_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, `HDL-C (mmol/L)`]
+    HDL_sd <- bio_ref_data[Variable == "SD" & Sex == gender, `HDL-C (mmol/L)`]
     if(length(HDL_mean) > 0 && length(HDL_sd) > 0){
       HDL_z <- calc_z(HDL_C, HDL_mean, HDL_sd)
       z_scores$HDL_C_z <- HDL_z
     }
   }
   
-  # Non-HDL Cholesterol (calculated if not provided)
-  if (is.null(non_HDL_C) && !is.null(total_cholesterol) && !is.null(HDL_C)) {
-    non_HDL_C <- total_cholesterol - HDL_C
-  }
-  
-  if (!is.null(non_HDL_C)) {
-    nonHDL_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, "Non_HDL_C"]
-    nonHDL_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender, "Non_HDL_C"]
-    if(length(nonHDL_mean) > 0 && length(nonHDL_sd) > 0){
-      nonHDL_z <- calc_z(non_HDL_C, nonHDL_mean, nonHDL_sd)
-      z_scores$Non_HDL_C_z <- nonHDL_z
-    }
-  }
-  
   # Triglycerides
   if (!is.null(triglycerides)) {
-    TG_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, "Triglycerides"]
-    TG_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender, "Triglycerides"]
+    TG_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, `TG (mmol/L)`]
+    TG_sd <- bio_ref_data[Variable == "SD" & Sex == gender, `TG (mmol/L)`]
     if(length(TG_mean) > 0 && length(TG_sd) > 0){
       TG_z <- calc_z(triglycerides, TG_mean, TG_sd)
       z_scores$Triglycerides_z <- TG_z
@@ -130,34 +107,16 @@ get_cardio_index <- function(age = 11,
   
   # Glucose
   if (!is.null(glucose)) {
-    Glu_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, "Glucose"]
-    Glu_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender, "Glucose"]
+    Glu_mean <- 
+      bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, `Glucose (mmol/L)`]
+    Glu_sd <- bio_ref_data[Variable == "SD" & Sex == gender, `Glucose (mmol/L)`]
     if(length(Glu_mean) > 0 && length(Glu_sd) > 0){
       Glu_z <- calc_z(glucose, Glu_mean, Glu_sd)
       z_scores$Glucose_z <- Glu_z
     }
   }
   
-  # Apolipoprotein A1
-  if (!is.null(apoA1)) {
-    ApoA1_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, "Apolipoprotein_A1"]
-    ApoA1_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender, "Apolipoprotein_A1"]
-    if(length(ApoA1_mean) > 0 && length(ApoA1_sd) > 0){
-      ApoA1_z <- calc_z(apoA1, ApoA1_mean, ApoA1_sd)
-      z_scores$ApoA1_z <- ApoA1_z
-    }
-  }
-  
-  # Apolipoprotein B
-  if (!is.null(apoB)) {
-    ApoB_mean <- bio_ref_data[Variable == "Mean" & Age_years == age & Sex == gender, "Apolipoprotein_B"]
-    ApoB_sd <- bio_ref_data[Variable == "SD" & Age_years == age & Sex == gender, "Apolipoprotein_B"]
-    if(length(ApoB_mean) > 0 && length(ApoB_sd) > 0){
-      ApoB_z <- calc_z(apoB, ApoB_mean, ApoB_sd)
-      z_scores$ApoB_z <- ApoB_z
-    }
-  }
-  
+
   # Calculate the Cardiovascular Index as the mean of Z-scores
   if(length(z_scores) == 0){
     warning("No valid variables provided for Cardiovascular Index calculation.")
