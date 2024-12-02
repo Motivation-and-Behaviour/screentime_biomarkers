568677edd614a59f29d39a38c4aada0c13dd4d48
{Taren Sanders} <{taren.sanders@acu.edu.au}>
2024-12-02 05:39:47 +0000

    Limit biomarkers file to just variables needed Fixes #19

--- R/read_biomarkers_data.R
+++ R/read_biomarkers_data.R
@@ -5,7 +5,40 @@
 #' @title
 #' @param filepath
 read_biomarkers_data <- function(filepath) {
-
-  foreign::read.spss(filepath, to.data.frame = TRUE, use.value.labels = TRUE)
-
+  raw_data <- foreign::read.spss(filepath,
+    to.data.frame = TRUE, use.value.labels = TRUE
+  )
+  raw_data %>%
+    dplyr::transmute(
+      vo2 = fcvo2mxm,
+      waistcm = fcwaist,
+      waist2height = fcwhrat,
+      bmiz = fcbmizc,
+      bodyfat = fcfatper,
+      pulsewavevel = fcpvavmn,
+      pulsepressamp = fcppamn,
+      bpsysamp = fcsbpamn,
+      bpsys = fcbrspmn,
+      bpdia = fcbrdpmn,
+      bpsysz = fcsbpzsc,
+      bpdiaz = fcdbpzsc,
+      phospholipids = fcb02c17l1,
+      cholestnonhdl = fcb02c11c1,
+      cholesttotal = fcb02c11a2,
+      cholesttotalhdl = fcb02c11e2,
+      glucose = fcb02c03a2,
+      glycoprotein = fcb02c042,
+      apolipa1 = fcb02c12a2,
+      apolipb = fcb02c12b2,
+      trigly = fcb02c14a2,
+      fastingtime = fcblfast,
+      sexualmaturity = fcsms,
+      servesveg = fch25c01a,
+      servesfruit = fch25c01b,
+      servessoftdrink = fch25c01h,
+      accvalidwkdays = fcvalwkday,
+      accvalidwedays = fcvalweday,
+      accmvpa = fca52mvtm,
+      accsed = fca52sdtm
+    )
 }
