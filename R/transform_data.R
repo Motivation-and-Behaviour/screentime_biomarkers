aa21db91529cf41484f5e42a35695beb1492a132
James Conigrave <james.conigrave@gmail.com>
2024-12-03 14:28:32 +1100

    add screentime sum, and transform to wide

--- R/transform_data.R
+++ R/transform_data.R
@@ -1,5 +1,9 @@
 transform_data <- function(scored_data) {
-  
-  scored_data
-
+  scored_data |>
+    dplyr::mutate_all(as.character) |>
+    tidyr::pivot_wider(
+      names_from = wave, # Create columns based on wave
+      values_from = -c(id, wave), # Keep id fixed, spread all other variables
+    names_sep = "_w"
+  )
 }
