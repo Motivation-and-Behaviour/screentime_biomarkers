ba050e8100bf5820d94c9b2076ee0a878ff141d2
James Conigrave <james.conigrave@gmail.com>
2024-12-03 13:54:34 +1100

    close issue #13

--- R/score_data.R
+++ R/score_data.R
@@ -1,4 +1,4 @@
-score_data <- function(df_clean) {
+score_data <- function(df_clean, bio_ref_data) {
 
   df_clean$ApoBA1_ratio <- df_clean$apolipa1 / df_clean$apolipb
   
@@ -6,8 +6,8 @@ score_data <- function(df_clean) {
   df_clean$diet_SSB_flag <- df_clean$servessoftdrink != "I don't drink soft drinks, cordials or sports drinks"
   # 2 or more serves of fruit
   df_clean$diet_fruit_flag <- as.numeric(df_clean$servesfruit) >= 4
-  # 5 or more serves of veg per day
+  # 5 or more serves of veg per day. Only 133 observations do!
   df_clean$diet_veg_flag <- as.numeric(df_clean$servesveg) >= 7
-
+  df_clean$diet <- ifelse(df_clean$diet_SSB_flag + df_clean$diet_fruit_flag + df_clean$diet_veg_flag == 3, "meets guidelines", "does not meet guidelines")
 
 }
