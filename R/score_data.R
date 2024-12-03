cd43567921411550d9f99a200b2696e0b68ea242
James Conigrave <james.conigrave@gmail.com>
2024-12-03 14:43:51 +1100

    fix scoring of diet

--- R/score_data.R
+++ R/score_data.R
@@ -3,10 +3,10 @@ score_data <- function(df_clean, bio_ref_data) {
 
   # Diet classificaiton
   df_clean$diet_SSB_flag <- df_clean$servessoftdrink != "I don't drink soft drinks, cordials or sports drinks"
-  # 2 or more serves of fruit
-  df_clean$diet_fruit_flag <- as.numeric(df_clean$servesfruit) >= 4
-  # 5 or more serves of veg per day. Only 133 observations do!
-  df_clean$diet_veg_flag <- as.numeric(df_clean$servesveg) >= 7
+  # 3 or more serves of fruit
+  df_clean$diet_fruit_flag <- as.numeric(df_clean$servesfruit) >= 5
+  # 4 or more serves of veg per day.
+  df_clean$diet_veg_flag <- as.numeric(df_clean$servesveg) >= 6
   df_clean$diet <- ifelse(df_clean$diet_SSB_flag + df_clean$diet_fruit_flag + df_clean$diet_veg_flag == 3, "meets guidelines", "does not meet guidelines")
 
   # Cardio score variable
