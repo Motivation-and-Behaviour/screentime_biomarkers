f10b95820a078fdae41d419ef8f296b530f67b29
James Conigrave <james.conigrave@gmail.com>
2024-12-03 14:30:32 +1100

    name obj within fn

--- R/transform_data.R
+++ R/transform_data.R
@@ -1,9 +1,10 @@
 transform_data <- function(scored_data) {
-  scored_data |>
+  transformed_data <- scored_data |>
     dplyr::mutate_all(as.character) |>
     tidyr::pivot_wider(
       names_from = wave, # Create columns based on wave
       values_from = -c(id, wave), # Keep id fixed, spread all other variables
     names_sep = "_w"
   )
+    transformed_data
 }
