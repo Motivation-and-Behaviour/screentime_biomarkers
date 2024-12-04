dad5808d62ab67d29968245f050d28f97ac605ac
{Taren Sanders} <{taren.sanders@acu.edu.au}>
2024-12-04 00:57:53 +0000

    Add mapped model building

--- R/fit_lgcm.R
+++ R/fit_lgcm.R
@@ -3,44 +3,79 @@
 #' .. content for \details{} ..
 #'
 #' @title
-#' @param transformed_data
-#' @param outcome
+#' @param transformed_data df. The transformed data.
+#' @param outcome character. The outcome variable.
+#' @param bloods boolean. Is the outcome from a blood test?
 #' @return
 #' @author {Taren Sanders}
 #' @export
-fit_lgcm <- function(transformed_data) {
+fit_lgcm <- function(transformed_data, outcome, bloods) {
+  covariates <- "sex_w6.5 + indig_w6 + ses_w6 + diet_w6.5 + sexualmaturity_w6.5"
+  if (bloods) {
+    covariates <- glue::glue("{covariates} + fastingtime_w6.5")
+  }
+
   model <- glue::glue(
     # nolint start
     "
     # LGCM
     intercept =~ 1 * st_total_w3 + 1 * st_total_w4 + 1 * st_total_w5 + 1 * st_total_w6
     slope =~ 0 * st_total_w3 + 1 * st_total_w4 + 2 * st_total_w5 + 3 * st_total_w6
+
+    # Variances and covariances
+    intercept ~~ intercept
+    slope ~~ slope
+    intercept ~~ slope
+
+    # Residual variances
+    st_total_w3 ~~ residual_var*st_total_w3
+    st_total_w4 ~~ residual_var*st_total_w4
+    st_total_w5 ~~ residual_var*st_total_w5
+    st_total_w6 ~~ residual_var*st_total_w6
+    residual_var > 0
+
+    # Regression of health outcome on latent factors and covariates
+    {outcome} ~ intercept + slope + {covariates}
     "
     # nolint end
   )
 
-  transformed_data <-
-    transformed_data %>%
-    filter(!is.na(st_total_w3) & !is.na(st_total_w4) & !is.na(st_total_w5) & !is.na(st_total_w6))
+  adj_model <- glue::glue(
+    # nolint start
+    "
+    # LGCM
+    intercept =~ 1 * st_total_w3 + 1 * st_total_w4 + 1 * st_total_w5 + 1 * st_total_w6
+    slope =~ 0 * st_total_w3 + 1 * st_total_w4 + 2 * st_total_w5 + 3 * st_total_w6
 
-  lgcm_fit <- lavaan::growth(model, data = transformed_data)
-  lavInspect(lgcm_fit, "fit")
-  lavInspect(lgcm_fit, "cov.lv")
+    # Variances and covariances
+    intercept ~~ intercept
+    slope ~~ slope
+    intercept ~~ slope
 
-  factor_scores <- lavaan::lavPredict(lgcm_fit)
+    # Residual variances
+    st_total_w3 ~~ residual_var*st_total_w3
+    st_total_w4 ~~ residual_var*st_total_w4
+    st_total_w5 ~~ residual_var*st_total_w5
+    st_total_w6 ~~ residual_var*st_total_w6
+    residual_var > 0
 
-  # Add factor scores to the data
-  transformed_data$intercept_factor <- factor_scores[, 1]
-  transformed_data$slope_factor <- factor_scores[, 2]
+    # Regression of health outcome on latent factors and covariates
+    {outcome} ~ intercept + slope + {covariates}
 
-  # Regress health outcome on the growth factors and covariates
-  model_with_covariates <- "
-    waistcm_w6.5 ~ intercept_factor + slope_factor + sex + indig + ses_w6
-  "
+    {outcome} ~ accmvpa_w6.5 + accsed_w6.5
+    "
+    # nolint end
+  )
+  model_outputs <- list()
 
-  # Fit the model
-  fit_with_covariates <- lavaan::sem(model_with_covariates, data = transformed_data)
+  model_outputs$lgcm_fit <- lavaan::growth(
+    model,
+    data = transformed_data, missing = "fiml"
+  )
+  model_outputs$lgcm_adj_fit <- lavaan::growth(
+    adj_model,
+    data = transformed_data, missing = "fiml"
+  )
 
-  # Summary
-  summary(fit_with_covariates, fit.measures = TRUE, standardized = TRUE)
+  model_outputs
 }
