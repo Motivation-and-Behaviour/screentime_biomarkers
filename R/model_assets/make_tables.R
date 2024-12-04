d946bc58edab0e623bca8dd68adaf4fd9bb64cc1
James Conigrave <james.conigrave@gmail.com>
2024-12-04 11:33:03 +1100

    add basic table making function

---
+++ R/model_assets/make_tables.R
@@ -0,0 +1,23 @@
+make_lgcm_table <- function(fit) {
+  require(lavaan)
+  est <- parameterEstimates(fit) |>
+    as.data.table()
+  tabby <- est[op == "~"]
+  tabby$pvalue <- metaKIN::round_p(tabby$pvalue)
+  tabby$est <- round(tabby$est, 2)
+  tabby$ci.lower <- round(tabby$ci.lower, 2)
+  tabby$ci.upper <- round(tabby$ci.upper, 2)
+  tabby$est_95 <- glue::glue("{tabby$est} [{tabby$ci.lower}, {tabby$ci.upper}]")
+  tabby$se <- round(tabby$se, 2)
+  tabby$z <- round(tabby$z, 2)
+  # assemble final table
+  tabby[, .(
+            Outcome = lhs,
+            Term = rhs,
+            Coef = est_95,
+            SE = se,
+            z = z,
+            p = pvalue
+            )]
+}
+
