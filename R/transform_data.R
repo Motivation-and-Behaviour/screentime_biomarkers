af45aea6b02e448cea43d59b4b7ad024178e1790
James Conigrave <james.conigrave@gmail.com>
2024-12-04 12:21:07 +1100

    add in bpsysamp

--- R/transform_data.R
+++ R/transform_data.R
@@ -46,6 +46,7 @@ transform_data <- function(scored_data, bio_ref_data, filter_valid = TRUE) {
       st_total_w6.5,
       bpdia_w6.5,
       bpsys_w6.5,
+      bpsysamp_w6.5,
       waistcm_w6.5,
       cholesttotalhdl_w6.5,
       cholesttotal_w6.5,
