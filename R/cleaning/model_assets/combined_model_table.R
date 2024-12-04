3de33394086d0f7182a4af69978b784c08327a50
James Conigrave <james.conigrave@gmail.com>
2024-12-04 14:08:27 +1100

    add combined model table

--- R/cleaning/model_assets/combined_model_table.R
+++ R/cleaning/model_assets/combined_model_table.R
@@ -1,3 +1,11 @@
 combined_model_table <- function(all_models) {
 
+  adj_tables <- lapply(all_models, function(m){
+    m$lgcm_adj_fit |>
+      make_lgcm_table_df()
+})
 
+
+  m[[2]] |> make_lgcm_table_df()
+
+}
