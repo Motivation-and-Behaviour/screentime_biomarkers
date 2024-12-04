d5b467d61875445a484540cbb08be6df723c6df4
{Taren Sanders} <{taren.sanders@acu.edu.au}>
2024-12-04 02:55:37 +0000

    Use tar_combine for fit measures

--- R/modelling/fit_measures_table.R
+++ R/modelling/fit_measures_table.R
@@ -11,25 +11,5 @@ fm <- function(i, title) {
 get_measures <- function(m, title) {
   out <- lapply(seq_along(m), function(i) fm(m[[i]], names(m)[i])) |>
     rbindlist()
-  out$model_name <- title
   out
-}
-
-fit_measures_table <- function(all_models) {
-  # get fit measures for all models
-  models <- lapply(
-    seq_along(all_models),
-    function(i) get_measures(all_models[[1]], title = names(all_models)[i])
-  )
-  fit_dat <- rbindlist(models)
-
-  # Load dplyr package
-  require(dplyr)
-
-  # Round numeric columns to 3 decimal places using mutate_if
-  fit_dat <- fit_dat |>
-    dplyr::mutate_if(is.numeric, round, 2) |>
-    dplyr::select(model_name, everything())
-
-  fit_dat
 }
