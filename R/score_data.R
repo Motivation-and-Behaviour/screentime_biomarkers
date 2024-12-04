6f47ba91efdc19ea320d53dcbd65ea729ab966dc
James Conigrave <james.conigrave@gmail.com>
2024-12-04 11:11:11 +1100

    make diet factor

--- R/score_data.R
+++ R/score_data.R
@@ -7,7 +7,10 @@ score_data <- function(df_clean) {
   df_clean$diet_fruit_flag <- as.numeric(df_clean$servesfruit) >= 5
   # 4 or more serves of veg per day.
   df_clean$diet_veg_flag <- as.numeric(df_clean$servesveg) >= 6
-  df_clean$diet <- ifelse(df_clean$diet_SSB_flag + df_clean$diet_fruit_flag + df_clean$diet_veg_flag == 3, "meets guidelines", "does not meet guidelines")
+  df_clean$diet <- ifelse(df_clean$diet_SSB_flag + df_clean$diet_fruit_flag + df_clean$diet_veg_flag == 3,
+    "meets guidelines", "does not meet guidelines"
+  ) |>
+    factor(levels = c("does not meet guidelines", "meets guidelines"))
 
   # Cardio score variable
   df_clean$age_integer <- as.integer(round(df_clean$age, 0))
