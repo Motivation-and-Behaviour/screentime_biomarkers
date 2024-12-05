53610cdde77101c010836f9643bee59e60fd3e38
Taren Sanders <taren.sanders@acu.edu.au>
2024-12-05 13:56:33 +0000

    Generate main outcome table

--- R/make_outcomes_table.R
+++ R/make_outcomes_table.R
@@ -1,12 +1,6 @@
 make_outcomes_table <- function(...) {
-  list(...)
+  theme_gtsummary_journal("jama", set_theme = TRUE)
+  
+  model_tables  <- list(...)
+  tbl_stack(model_tables)
 }
-
-theme_gtsummary_journal("jama", set_theme = TRUE)
-tbl_stack(outcomes_table)
-
-class(outcomes_table[[1]])
-
-names(outcomes_table)
-
-unlist(outcomes_table)
