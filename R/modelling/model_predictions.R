6029d697f7269fad3e2c65ad540089b7359c7365
James Conigrave <james.conigrave@gmail.com>
2024-12-05 13:18:58 +1100

    make mean predictions without standard errors

--- R/modelling/model_predictions.R
+++ R/modelling/model_predictions.R
@@ -2,36 +2,32 @@ make_model_predictions <- function(m = model_vo2_w6.5, transformed_data) {
   require(lavaan)
   covariates <- c("female", "indig", "ses_w6", "bad_diet", "sexualmaturity_numeric_w6.5")
   outcome <- attr(m, "outcome")
-browser()  
-plot_dat <- lapply(seq_along(m), function(x){
+  plot_dat <- lapply(seq_along(m), function(x){
            make_prediction(
                            m[[x]],
                            type = names(m)[x],
                            vals = c(-2,0,2),
                            outcome,
+                           covariates,
                            transformed_data)
-})
-
-  m <- m$lgcm_adj_fit
-
+              })
+  plot_dat
 }
 
-make_prediction <- function(m, type, vals = c(-2,0,2), outcome, transformed_data) {
+make_prediction <- function(m, type, vals = c(-2,0,2), outcome, covariates, transformed_data) {
 
-pred_data <- transformed_data[1:9,]
-st_vars <- grep("^st.*_scaled", colnames(pred_data), value = TRUE)
+  pred_data <- transformed_data[1:9,]
+  st_vars <- grep("^st.*_scaled", colnames(pred_data), value = TRUE)
 
-# Set all screen time variables to
-pred_data_low <- pred_data
-pred_data_low[, st_vars] <- rep(vals,3)
-pred_data_low[, covariates] <- 0
+  # Set all screen time variables to
+  pred_data_low <- pred_data
+  pred_data_low[, st_vars] <- rep(vals,3)
+  pred_data_low[, covariates] <- 0
 
-preds <- lavPredictY(m,
-            newdata = pred_data_low,
-            ynames = outcome,
-            xnames = c(st_vars, covariates),
-           )
+  preds <- lavPredictY(m,
+    newdata = pred_data_low,
+    ynames = lavNames(m, "ov.y"),
+    xnames = c(st_vars, covariates),
+    )
 data.frame(outcome = outcome, mean = preds[1:3], type = type, st_vals = vals)
-
-
 }
