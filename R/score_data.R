ff6bc62f1276a927037f9cae93ade36bafb7c8d5
James Conigrave <12742211+conig@users.noreply.github.com>
2026-07-30 12:35:32 +1000

    fix issue with diet flag (#75)
    * fix issue with diet flag
    
    * remove comment
    
    * remove another comment
    
    ---------
    
    Co-authored-by: conig <jamesconigrave@gmail.com>

--- R/score_data.R
+++ R/score_data.R
@@ -1,7 +1,7 @@
 score_data <- function(df_clean) {
   df_clean$ApoBA1_ratio <- df_clean$apolipb / df_clean$apolipa1
 
-  df_clean$diet_SSB_flag <- df_clean$servessoftdrink != "I don't drink soft drinks, cordials or sports drinks"
+  df_clean$diet_SSB_flag <- df_clean$servessoftdrink == "I don't drink soft drinks, cordials or sports drinks"
   df_clean$diet_fruit_flag <- as.numeric(df_clean$servesfruit) >= 5
   df_clean$diet_veg_flag <- as.numeric(df_clean$servesveg) >= 6
   df_clean$diet <- ifelse(df_clean$diet_SSB_flag + df_clean$diet_fruit_flag + df_clean$diet_veg_flag == 3,
