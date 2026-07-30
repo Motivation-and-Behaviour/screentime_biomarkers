ad8b218bd0cba034190127373e879112c415b9df
conig <jamesconigrave@gmail.com>
2026-07-30 11:18:14 +1000

    remove another comment

--- R/score_data.R
+++ R/score_data.R
@@ -1,7 +1,6 @@
 score_data <- function(df_clean) {
   df_clean$ApoBA1_ratio <- df_clean$apolipb / df_clean$apolipa1
 
-  # Diet classificaiton
   df_clean$diet_SSB_flag <- df_clean$servessoftdrink == "I don't drink soft drinks, cordials or sports drinks"
   df_clean$diet_fruit_flag <- as.numeric(df_clean$servesfruit) >= 5
   df_clean$diet_veg_flag <- as.numeric(df_clean$servesveg) >= 6
